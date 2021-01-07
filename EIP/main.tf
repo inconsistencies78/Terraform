12b2dafedb963991488a5401aa1dfe1078365dc7
Andreas Barthel <uprightcitizen@gmx.de>
2021-01-07 10:54:28 +0100

    EIP

---
+++ EIP/main.tf
@@ -0,0 +1,39 @@
+# udemy kurs Terraform on AWS. Abschnitt 5. EC2
+# mehr Infos zu den Instances unter:
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+# Infos zu t2.micro als Instance unter:
+# https://aws.amazon.com/de/ec2/instance-types/
+# https://aws.amazon.com/de/ec2/instance-types/t2/
+# ami von EC2 unter:
+# https://eu-central-1.console.aws.amazon.com/ec2/v2/home?region=eu-central-1#LaunchInstanceWizard:
+
+
+provider "aws" {
+    region = "eu-central-1"
+}
+
+resource "aws_instance" "ec2" {
+  ami = "ami-03c3a7e4263fd998c"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "1st-EC2"
+  }
+}
+
+
+# Elastic IP
+# um keine sich ändernde IP nach außen zu haben.
+# mehr Infos unter:
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip
+# wir wollen hier die EIP mit unserer EC2 Instance verknüpfen
+# 
+
+# 
+resource "aws_eip" "elasticeip" {  # wir erstellen eine EIP mit dem Namen "elasticeip"
+  instance = aws_instance.ec2.id  # optionale Info. wie hier angegeben verknüpft es EIP mit unserem EC2
+# Schema: instance = resourcename.name.attribut
+}
+
+output "EIP" {
+  value = aws_eip.elasticeip.public_ip  # 
+}
